dd271692fd479f50aeaf64de64e91f84681404fc
Notsky <notsky@gmail.com>
2024-04-04 15:46:59 -0400

    A5-Client

--- src/Labs/a5/EncodingParametersInURLs.tsx
+++ src/Labs/a5/EncodingParametersInURLs.tsx
@@ -5,8 +5,8 @@ function EncodingParametersInURLs() {
   const [a, setA] = useState(34);
   const [b, setB] = useState(23);
   const [welcome, setWelcome] = useState("");
-  // const API_BASE = process.env.REACT_APP_API_BASE;
-  const API_BASE = "https://kanbas-node-server-app-vvg4.onrender.com";
+  const API_BASE = process.env.REACT_APP_API_BASE;
+  // const API_BASE = "https://kanbas-node-server-app-vvg4.onrender.com";
 
   const fetchWelcome = async () => {
     // const response = await axios.get("http://localhost:4000/a5/welcome");
