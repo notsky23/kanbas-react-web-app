78f2482d0d7a355cf8bc1c6e6ab94cca427ab485
Notsky <notsky@gmail.com>
2024-04-04 15:32:13 -0400

    A5-Client

--- src/Labs/a5/index.tsx
+++ src/Labs/a5/index.tsx
@@ -12,7 +12,7 @@ function Assignment5() {
       <div className="container list-group">
         <h1>Assignment 5</h1>
         {/* <a className="list-group-item list-group-item-action" href="http://localhost:4000/a5/welcome"> */}
-        <a className="list-group-item list-group-item-action" href={WELCOME_API}>
+        <a className="list-group-item list-group-item-action" href={`${API_BASE}/a5/welcome`}>
           Welcome
         </a> <br />
         <EncodingParametersInURLs /> <br />
